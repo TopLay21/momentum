47cda682e0270445f3fe6bc53723d171d9391757
TopLay21 <135070055+TopLay21@users.noreply.github.com>
2023-09-15 18:44:20 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -7,19 +7,18 @@ const USERNAME_KEY = "username"; //만약 오타를 낸다면 자바스크립트
 
 function onLoginSubmit(event){
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(savedUsername);
+    paintGreetings(username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
 }
 
 function paintGreetings(username) {
-    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-//console.log(savedUsername);
 
 if(savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
