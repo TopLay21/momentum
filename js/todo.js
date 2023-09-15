a9032e049a381880be33b62719b4fa4e259404c2
TopLay21 <135070055+TopLay21@users.noreply.github.com>
2023-09-15 21:16:56 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -24,7 +24,7 @@ function paintToDo(newTodo) {
     const span = document.createElement("span");
     span.innerText = newTodo.text;
     const button = document.createElement("button");
-    button.innerText = "❌"
+    button.innerText = "🆇"
     button.addEventListener("click", deleteToDo);
     li.appendChild(span);
     li.appendChild(button);
