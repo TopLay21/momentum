10c1e1b437b7bf46c6b4b6d06b17bff0ead42a84
TopLay21 <135070055+TopLay21@users.noreply.github.com>
2023-11-06 14:49:58 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -2,12 +2,6 @@ const images = ["0.webp", "1.jpg", "2.jpg", "3.jpg", "4.jpeg", "5.jpg"];
 
 const chosenImages = images[Math.floor(Math.random() * images.length)];
 
-//const bgImage = document.createElement("img");
-
-//bgImage.src = `img/${chosenImages}`;
-
-//document.body.appendChild(bgImage);
-
 const body = document.body;
 body.style.backgroundImage = `url('img/${chosenImages}')`;
 body.style.backgroundSize = 'cover';
