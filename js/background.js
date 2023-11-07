11ed8744245ca617145c1f8672bb7860679400ca
TopLay21 <135070055+TopLay21@users.noreply.github.com>
2023-11-08 03:31:41 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,17 @@
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpeg", "5.jpg", "6.jpg", "7.png", "8.jpg", "9.jpg"];
+const preloadedImages = [];
 
-const chosenImages = images[Math.floor(Math.random() * images.length)];
+for (const imageSrc of images) {
+  const img = new Image();
+  img.src = `img/${imageSrc}`;
+  preloadedImages.push(img);
+}
+
+const randomIndex = Math.floor(Math.random() * preloadedImages.length);
+const chosenImage = preloadedImages[randomIndex];
 
 const body = document.body;
-body.style.backgroundImage = `url('img/${chosenImages}')`;
+body.style.backgroundImage = `url('${chosenImage.src}')`;
 body.style.backgroundSize = 'cover';
 body.style.backgroundPosition = 'center';
 body.style.backgroundRepeat = 'no-repeat';
